035115aea63afbc262feca9888b5754dcf5ad60d
Primemaster <cesarsky5@gmail.com>
2021-08-05 20:51:00 +0100

    Midi keys test. :D

---
+++ funny_keys/keys.js
@@ -0,0 +1,121 @@
+/*
+This is a p5js sketch that uses the WebMIDI.js library to receive MIDI messages
+*/
+
+//background color variable
+let bgColor;
+const SIZE_X = 1000;
+
+//text to be displayed
+let displayText;
+
+function NoteQueue()
+{
+    this.frame = 0;
+    this.notes = {};
+    this.push = function(id)
+    {
+        if(id in this.notes)
+        {
+            let d = { on: 0, startFrame: this.frame };
+            this.notes[id].push(d);
+        }
+        else
+        {
+            this.notes[id] = [];
+            this.notes[id].push({});
+            this.notes[id][0].on = 0;
+            this.notes[id][0].startFrame = this.frame;
+        }
+    }
+    
+    this.pop = function(id)
+    {
+        if(id in this.notes)
+        {
+            for(let iv = 0; iv < this.notes[id].length; iv++)
+            {
+                if( this.notes[id][iv].on === 0)
+                {
+                    this.notes[id][iv].on = 1;
+                    this.notes[id][iv].endFrame = this.frame;
+                    this.notes[id][iv].counter = 0;
+                    break;
+                }
+            }
+        }
+    }
+
+    this.draw = function()
+    {
+        fill(255);
+        for(var k in this.notes)
+        {
+            for(let iv = 0; iv < this.notes[k].length; iv++)
+            {
+                if(this.notes[k][iv].on === 0)
+                {
+                    for(let i = 0; i < this.frame - this.notes[k][iv].startFrame; i++)
+                    {
+                        rect(k * 10, 10 * i, 10, 10);
+                    }
+                }
+                else if(this.notes[k][iv].on === 1)
+                {
+                    if(this.notes[k][iv].counter > 700)
+                    {
+                        this.notes[k][iv].on = 2; // Dispose
+                    }
+
+                    for(let i = 0; i < this.notes[k][iv].endFrame - this.notes[k][iv].startFrame; i++)
+                    {
+                        rect(k * 10, 10 * (i + this.notes[k][iv].counter), 10, 10);
+                    }
+
+                    this.notes[k][iv].counter++;
+                }
+                else
+                {
+                    this.notes[k].shift();
+                }
+            }
+        }
+        this.frame++;
+    }
+}
+
+let nq;
+
+function setup() 
+{
+    nq = new NoteQueue();
+    //400 by 400 pixel canvas
+    createCanvas(SIZE_X, 700);
+
+    WebMidi.enable(function (err) { //check if WebMidi.js is enabled
+        if (err) {
+            console.log("WebMidi could not be enabled.", err);
+        } else {
+            console.log("WebMidi enabled!");
+        }
+
+        //Choose an input port
+        inputSoftware = WebMidi.inputs[0];
+
+        inputSoftware.addListener('noteon', "all", function (e) {
+            nq.push(e.note.number);
+        });
+
+        inputSoftware.addListener('noteoff', "all", function (e) {
+            nq.pop(e.note.number);
+        });
+	});
+}
+
+function draw() { 
+    //Draw background with background color variable
+    //Will change every time there is a note on
+    //background(bgColor);
+    background(0);
+    nq.draw();
+}
